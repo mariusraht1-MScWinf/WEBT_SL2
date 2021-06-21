add210dccf41d3c3835b0a98b080cae6bff20223
mariusraht2 <mariusraht+1@gmail.com>
2021-06-21 11:59:26 +0200

    Country select, pretty printer

--- js/vaccinations_over_time.js
+++ js/vaccinations_over_time.js
@@ -5,22 +5,23 @@ class VaccinationsOverTime {
       .then((json) => json.sort((a, b) => d3.ascending(a.country, b.country)))
       .then((json) => {
         console.log(json);
-        d3.select("#countries")
-          .selectAll("option")
-          .data(json)
-          .enter()
-          .append("option")
-          .text((d) => d.country);
+
+        json.forEach((entry) => {
+          let option = document.createElement("option");
+          option.text = entry.country;
+          option.dataset.code = entry.code;
+          document.querySelector("#countries").append(option);
+        });
+
+        d3.select("#countries").selectAll("option").data(json);
       });
   }
 
   static create_chart() {
-    // set the dimensions and margins of the graph
     var margin = { top: 20, right: 20, bottom: 40, left: 60 },
       width = 460 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
-    // create svg element, respecting margins
     var svg = d3
       .select("#chart")
       .append("svg")
@@ -29,32 +30,37 @@ class VaccinationsOverTime {
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Add X axis
     var x = d3.scaleLinear().domain([0, 100]).range([0, width]);
     svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-    // Add Y axis
     var y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
 
-    // Add X axis label:
     svg
       .append("text")
       .attr("text-anchor", "end")
-      .attr("x", width)
+      .attr("x", width / 2 + 24)
       .attr("y", height + margin.top + 20)
       .text("Datum");
 
-    // Y axis label:
     svg
       .append("text")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.left + 20)
-      .attr("x", -height / 4)
+      // .attr("text-anchor", "end")
+      .attr("transform", "rotate(90)")
+      .attr("y", margin.left - 20)
+      .attr("x", height / 4)
       .text("Impfungen (kummuliert)");
+  }
+
+  static showData(code) {
+    console.log(`Show data by code ${code}...`);
+  }
+
+  static onChangeSelectCountries(selectedItem) {
+    let code = selectedItem.selectedOptions[0].dataset.code;
+    VaccinationsOverTime.showData(code);
   }
 }
