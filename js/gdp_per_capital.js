c93cc1da2e1e886edbb79899b5c2262ff95a627d
mariusraht2 <mariusraht+1@gmail.com>
2021-06-23 12:39:45 +0200

    Pie Chart

--- js/gdp_per_capital.js
+++ js/gdp_per_capital.js
@@ -1,6 +1,6 @@
 class GdpPerCapital {
     static test() {
-        // Pie Chart: https://www.d3-graph-gallery.com/graph/pie_basic.html
+        // Pie Chart: https://www.d3-graph-gallery.com/graph/pie_basic.html und https://observablehq.com/@d3/pie-chart
         // Layout: https://getbootstrap.com/docs/5.0/layout/grid/
 
         console.log("Hello World");
