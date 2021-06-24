c41c5d24030bab60af591bb601360cfd96994e05
ZdravkaT <zdravka.todorova80@gmail.com>
2021-06-24 14:56:57 +0200

    test

--- js/gdp_per_capital.js
+++ js/gdp_per_capital.js
@@ -1,8 +1,51 @@
-class GdpPerCapital {
-    static test() {
-        // Pie Chart: https://www.d3-graph-gallery.com/graph/pie_basic.html und https://observablehq.com/@d3/pie-chart
-        // Layout: https://getbootstrap.com/docs/5.0/layout/grid/
-
-        console.log("Hello World");
+class GdpPerCapita {
+    static svg;
+    static groupKey = "date";
+      
+    static fillCountriesSelect() {
+      Countries.getData().then((json) => {
+        console.log(json);
+  
+        json.forEach((entry) => {
+          let option = document.createElement("option");
+          option.text = entry.country;
+          option.dataset.code = entry.code;
+          document.querySelector("#countries").append(option);
+        });
+  
+        d3.select("#countries").selectAll("option").data(json);
+      });
+    } 
+    static showData(code) {
+        fetch(`https://l1n.de/tl2/public/country/${code}/gdp_per_capita`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            GdpPerCapita.create_chart(data);
+          });
+      }
+    
+      static onChangeSelectCountries(selectedItem) {
+        let code = selectedItem.selectedOptions[0].dataset.code;
+        GdpPerCapita.showData(code);
+      }
+    
+      static showLoader(show = false) {
+        switch (show) {
+          case true:
+            document.querySelector("#loader_vaccinations_over_time").classList.remove("d-none");
+            document.querySelector("#loader_vaccinations_over_time").classList.add("d-inline");
+            break;
+          case false:
+            document.querySelector("#loader_vaccinations_over_time").classList.add("d-none");
+            break;
+        }
+      }
     }
-}
+      
+    
+
+                          
+              
+                  
+                   
