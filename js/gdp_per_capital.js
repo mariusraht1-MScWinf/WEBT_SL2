0c61129f1bccbb3ca473151ab5e1b10f389792a0
ZdravkaT <zdravka.todorova80@gmail.com>
2021-06-24 15:30:32 +0200

    test

--- js/gdp_per_capital.js
+++ js/gdp_per_capital.js
@@ -1,51 +1,6 @@
-class GdpPerCapita {
-    static svg;
-    static groupKey = "date";
-      
-    static fillCountriesSelect() {
-      Countries.getData().then((json) => {
-        console.log(json);
-  
-        json.forEach((entry) => {
-          let option = document.createElement("option");
-          option.text = entry.country;
-          option.dataset.code = entry.code;
-          document.querySelector("#countries").append(option);
-        });
-  
-        d3.select("#countries").selectAll("option").data(json);
-      });
-    } 
-    static showData(code) {
-        fetch(`https://l1n.de/tl2/public/country/${code}/gdp_per_capita`)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            GdpPerCapita.create_chart(data);
-          });
-      }
-    
-      static onChangeSelectCountries(selectedItem) {
-        let code = selectedItem.selectedOptions[0].dataset.code;
-        GdpPerCapita.showData(code);
-      }
-    
-      static showLoader(show = false) {
-        switch (show) {
-          case true:
-            document.querySelector("#loader_vaccinations_over_time").classList.remove("d-none");
-            document.querySelector("#loader_vaccinations_over_time").classList.add("d-inline");
-            break;
-          case false:
-            document.querySelector("#loader_vaccinations_over_time").classList.add("d-none");
-            break;
-        }
-      }
-    }
-      
-    
+class GdpPerCapita {}
 
-                          
+   
               
                   
                    
