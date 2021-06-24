71fbfd87318b3324292da4a83b4c9a5b6d69d5cb
ZdravkaT <zdravka.todorova80@gmail.com>
2021-06-24 20:25:50 +0200

    test

--- js/countries.js
+++ js/countries.js
@@ -7,3 +7,13 @@ class Countries {
       .then((json) => json.sort((a, b) => d3.ascending(a.country, b.country)));
   }
 }
+
+class GdpPerCapita {
+  data = [];
+
+  static getData() {
+    return fetch("data/GdpPerCapita.json")
+      .then((response) => response.json())
+      .then((json) => json.sort((a, b) => d3.ascending(a.country, b.country)));
+  }
+}
--- js/gdp_per_capita.js
+++ js/gdp_per_capita.js
@@ -1,4 +1,72 @@
 
+class GdpPerCapita {
+    static svg;
+}
+function GdpPerCapita(values) {
+
+    var chart = new CanvasJS.Chart("GdpPerCapita", {
+      backgroundColor: "white",
+      colorSet: "colorSet2",
+  
+      title: {
+        text: "GdpPerCapita",
+               fontFamily: "Verdana",
+        fontSize: 25,
+        fontWeight: "normal",
+      },
+      animationEnabled: true,
+      data: [{
+        indexLabelFontSize: 15,
+        indexLabelFontFamily: "Monospace",
+        indexLabelFontColor: "darkgrey",
+        indexLabelLineColor: "darkgrey",
+        indexLabelPlacement: "outside",
+        type: "pie",
+        showInLegend: false,
+        toolTipContent: "<strong>#percent%</strong>",
+        dataPoints: values
+      }]
+    });
+    chart.render();
+  }
+
+  renderGdpPerCapita(GdpPerCapitaValues);
+  
+  function renderGdpPerCapita(values) {
+  
+    var chart = new CanvasJS.Chart("GdpPerCapita", {
+      backgroundColor: "white",
+      colorSet: "colorSet3",
+      title: {
+        text: "GdpPerCapita",
+        fontFamily: "Verdana",
+        fontSize: 25,
+        fontWeight: "normal",
+      },
+      animationEnabled: true,
+      legend: {
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+      },
+      theme: "theme2",
+      data: [
+  
+        {
+          indexLabelFontSize: 15,
+          indexLabelFontFamily: "Monospace",
+          indexLabelFontColor: "darkgrey",
+          indexLabelLineColor: "darkgrey",
+          indexLabelPlacement: "outside",
+          type: "column",
+          showInLegend: false,
+          legendMarkerColor: "grey",
+          dataPoints: values
+        }
+      ]
+    });
+  
+    chart.render();
+  }
        
 
 
