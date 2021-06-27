89d4e4829825099854dd451cfc9bf7bec438616a
ZdravkaT <zdravka.todorova80@gmail.com>
2021-06-27 20:57:13 +0200

    test

--- js/gdp_per_capita.js
+++ js/gdp_per_capita.js
@@ -1,23 +1,23 @@
 class GdpPerCapita {
   static createChart() {
-    var data = [2, 4, 8, 10];
+    let data = [2, 4, 8, 10];
 
-    var svg = d3.select("#gdp_per_capita"),
+    let svg = d3.select("#gdp_per_capita"),
       width = svg.attr("width"),
       height = svg.attr("height"),
       radius = Math.min(width, height) / 2,
       g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var color = d3.scaleOrdinal(["#4daf4a", "#377eb8", "#ff7f00", "#984ea3", "#e41a1c"]);
+    let color = d3.scaleOrdinal(["#4daf4a", "#377eb8", "#ff7f00", "#984ea3", "#e41a1c"]);
 
     // Generate the pie
-    var pie = d3.pie();
+    let pie = d3.pie();
 
     // Generate the arcs
-    var arc = d3.arc().innerRadius(0).outerRadius(radius);
+    let arc = d3.arc().innerRadius(0).outerRadius(radius);
 
     //Generate groups
-    var arcs = g.selectAll("arc").data(pie(data)).enter().append("g").attr("class", "arc");
+    let arcs = g.selectAll("arc").data(pie(data)).enter().append("g").attr("class", "arc");
 
     //Draw arc paths
     arcs
