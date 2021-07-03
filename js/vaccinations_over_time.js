5b19f70f6984a61d3c2315102d233145dd959fe9
mariusraht2 <mariusraht+1@gmail.com>
2021-07-03 22:29:28 +0200

    Grouped bar chart (tbc)

--- js/vaccinations_over_time.js
+++ js/vaccinations_over_time.js
@@ -28,61 +28,142 @@ class VaccinationsOverTime {
   static createChart(data) {
     d3.select("#vaccinations_over_time > *").remove();
 
-    let svg = d3.select("#vaccinations_over_time"),
-      margin = { top: 20, right: 20, bottom: 40, left: 90 },
+    let groupedData = [];
+    data.forEach((d) => {
+      groupedData.push({
+        key: new Date(d.date),
+        values: [
+          { name: "first", value: d.new_vaccinations },
+          { name: "complete", value: d.people_fully_vaccinated },
+        ],
+      });
+    });
+
+    let margin = { top: 20, right: 20, bottom: 30, left: 70 },
       width = 800 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
-    svg.attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom);
+    var x0 = d3.scaleBand().rangeRound([0, width], 0.5);
+    var x1 = d3.scaleBand();
+    var y = d3.scaleLinear().rangeRound([height, 0]);
 
-    let xScale = d3
-      .scaleBand()
-      .range([0, width])
-      .padding(0.4)
-      .domain(data.map((d) => d.date));
+    var xAxis = d3
+      .axisBottom()
+      .scale(x0)
+      .tickFormat(d3.timeFormat("%-m/%Y"))
+      .tickValues(groupedData.map((d) => d.key));
 
-    let yScale = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([0, d3.max(data, (d) => d.total_vaccinations)]);
+    var yAxis = d3.axisLeft().scale(y);
 
-    let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const color = d3.scaleOrdinal(["lightblue", "lightgreen"]);
 
-    g.append("g")
+    let svg = d3
+      .select("#vaccinations_over_time")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var categoriesNames = groupedData.map((d) => d.key);
+    var rateNames = groupedData[0].values.map((gd) => gd.name);
+
+    x0.domain(categoriesNames);
+    x1.domain(rateNames).rangeRound([0, x0.bandwidth()]);
+    y.domain([0, d3.max(groupedData, (key) => d3.max(key.values, (gd) => gd.value))]);
+
+    svg
+      .append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale))
-      .append("text")
-      .attr("x", width / 2 + 24)
-      .attr("y", margin.bottom)
-      .attr("text-anchor", "end")
-      .attr("fill", "black")
-      .attr("font-size", "14px")
-      .text("Datum");
+      .call(xAxis);
 
-    g.append("g")
-      .call(d3.axisLeft(yScale).ticks(10))
-      .append("text")
-      .attr("transform", "rotate(90)")
-      .attr("y", 80)
-      .attr("x", height / 2 + 2 * margin.top + margin.bottom)
-      .attr("fill", "black")
-      .attr("font-size", "14px")
-      .text("Impfungen (kummuliert)");
+    svg.append("g").attr("class", "y axis").style("opacity", "0").call(yAxis);
 
-    g.selectAll("rect")
-      .data(data)
+    svg.select(".y").style("opacity", "1");
+
+    let slice = svg
+      .selectAll(".slice")
+      .data(groupedData)
+      .enter()
+      .append("g")
+      .attr("class", "g")
+      .attr("transform", (d) => "translate(" + x0(d.key) + ",0)");
+
+    slice
+      .selectAll("rect")
+      .data((d) => d.values)
       .enter()
       .append("rect")
-      .attr("x", (d) => xScale(d.date))
-      .attr("y", (d) => yScale(d.total_vaccinations))
-      .attr("width", xScale.bandwidth())
-      .attr("height", (d) => height - yScale(d.total_vaccinations))
-      .attr("fill", "green");
+      .attr("width", x1.bandwidth())
+      .style("fill", (gd) => color(gd.name))
+      .attr("x", (gd) => x1(gd.name))
+      .attr("y", (gd) => y(0))
+      .attr("height", (gd) => height - y(0));
 
-    VaccinationsOverTime.showLoader(false);
+    slice
+      .selectAll("rect")
+      .attr("y", (gd) => y(gd.value))
+      .attr("height", (gd) => height - y(gd.value));
   }
 
- /* static createTable(data) {
+  // static createChart(data) {
+  //   d3.select("#vaccinations_over_time > *").remove();
+
+  //   let svg = d3.select("#vaccinations_over_time"),
+  //     margin = { top: 20, right: 20, bottom: 40, left: 90 },
+  //     width = 800 - margin.left - margin.right,
+  //     height = 400 - margin.top - margin.bottom;
+
+  //   svg.attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom);
+
+  //   let xScale = d3
+  //     .scaleBand()
+  //     .range([0, width])
+  //     .padding(0.4)
+  //     .domain(data.map((d) => d.date));
+
+  //   let yScale = d3
+  //     .scaleLinear()
+  //     .range([height, 0])
+  //     .domain([0, d3.max(data, (d) => d.total_vaccinations)]);
+
+  //   let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //   g.append("g")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(d3.axisBottom(xScale))
+  //     .append("text")
+  //     .attr("x", width / 2 + 24)
+  //     .attr("y", margin.bottom)
+  //     .attr("text-anchor", "end")
+  //     .attr("fill", "black")
+  //     .attr("font-size", "14px")
+  //     .text("Datum");
+
+  //   g.append("g")
+  //     .call(d3.axisLeft(yScale).ticks(10))
+  //     .append("text")
+  //     .attr("transform", "rotate(90)")
+  //     .attr("y", 80)
+  //     .attr("x", height / 2 + 2 * margin.top + margin.bottom)
+  //     .attr("fill", "black")
+  //     .attr("font-size", "14px")
+  //     .text("Impfungen (kummuliert)");
+
+  //   g.selectAll("rect")
+  //     .data(data)
+  //     .enter()
+  //     .append("rect")
+  //     .attr("x", (d) => xScale(d.date))
+  //     .attr("y", (d) => yScale(d.total_vaccinations))
+  //     .attr("width", xScale.bandwidth())
+  //     .attr("height", (d) => height - yScale(d.total_vaccinations))
+  //     .attr("fill", "green");
+
+  //   VaccinationsOverTime.showLoader(false);
+  // }
+
+  /* static createTable(data) {
     let table = document.getElementById("vaccination_table");
     table.querySelectorAll("td:not(:first-child), th:not(:first-child)").forEach((x) => x.remove());
     data.forEach(function (item, index) {
@@ -105,20 +186,20 @@ class VaccinationsOverTime {
     let table = document.getElementById("vaccination_table");
     table.querySelectorAll("td:not(:first-child), th:not(:first-child)").forEach((x) => x.remove());
     d3.formatDefaultLocale({
-      "decimal": ",",
-      "grouping": [3],
-      "thousands": ".",
-      "currency": "€"
-    })
+      decimal: ",",
+      grouping: [3],
+      thousands: ".",
+      currency: "€",
+    });
     data.forEach(function (item, index) {
       let t = document.createElement("th");
       t.innerHTML = item.date;
       table.querySelector("thead tr").appendChild(t);
       t = document.createElement("td");
-      t.innerHTML = d3.format(",")(item.people_fully_vaccinated)//item.people_fully_vaccinated  == undefined ? 0 : item.people_fully_vaccinated; // in case the intervall is set to 1
+      t.innerHTML = d3.format(",")(item.people_fully_vaccinated); //item.people_fully_vaccinated  == undefined ? 0 : item.people_fully_vaccinated; // in case the intervall is set to 1
       table.querySelector("tbody tr:first-child").appendChild(t);
       t = document.createElement("td");
-      t.innerHTML = d3.format(",")(item.new_vaccinations)//  == undefined ? "unknown" : item.new_vaccinations; // in case the intervall is set to 1
+      t.innerHTML = d3.format(",")(item.new_vaccinations); //  == undefined ? "unknown" : item.new_vaccinations; // in case the intervall is set to 1
       table.querySelector("tbody tr:nth-child(2)").appendChild(t);
       t = document.createElement("td");
       t.innerHTML = d3.format(",")(item.total_vaccinations);
@@ -126,8 +207,7 @@ class VaccinationsOverTime {
     });
   }
 
-
-/*
+  /*
   static showData(code) {
     fetch(`https://l1n.de/tl2/public/country/${code}/vaccinations`)
       .then((response) => response.json())
@@ -180,23 +260,23 @@ class VaccinationsOverTime {
       });
   }*/
 
-  static showData (code) {
+  static showData(code) {
     fetch(`https://l1n.de/tl2/public/country/${code}/vaccinations`)
       .then((response) => response.json())
-      .then((data) => { 
-        console.log(data)
-        let aggregateddata = []
-        d3.group(data, d => d.date.toString().substring(0,7)).forEach(function (item,index){
-          let month = item[0].date.toString().substring(0,7)
-          aggregateddata.push({
-            'date': month,
-            'new_vaccinations': d3.sum(item, d => d.new_vaccinations),
-            'people_fully_vaccinated': d3.max(item, d => d.people_fully_vaccinated),
-            'total_vaccinations': d3.max(item, d => d.total_vaccinations)
-          })
-        })
-        VaccinationsOverTime.createChart(aggregateddata);
-        VaccinationsOverTime.createTable(aggregateddata);
+      .then((data) => {
+        console.log(data);
+        let aggregatedData = [];
+        d3.group(data, (d) => d.date.toString().substring(0, 7)).forEach(function (item, index) {
+          let month = item[0].date.toString().substring(0, 7);
+          aggregatedData.push({
+            date: month,
+            new_vaccinations: d3.sum(item, (d) => d.new_vaccinations),
+            people_fully_vaccinated: d3.max(item, (d) => d.people_fully_vaccinated),
+            total_vaccinations: d3.max(item, (d) => d.total_vaccinations),
+          });
+        });
+        VaccinationsOverTime.createChart(aggregatedData);
+        VaccinationsOverTime.createTable(aggregatedData);
       });
   }
 
